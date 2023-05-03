29912618e5cdde28bc32b4817fd428081bc85db9
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-03 16:46:31 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
+
 opts = Options()
 opts.set_headless()
+
 assert opts.headless  # Operating in headless mode
 browser = Firefox(options=opts)
 browser.get('http://www.tsetmc.com/Loader.aspx?ParTree=15')
