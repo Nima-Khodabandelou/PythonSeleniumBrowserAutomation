b94741d549e81ae475232dd1d47a59f4a6b879a2
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-21 11:09:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ opts = Options()
 opts.set_headless()
 
 assert opts.headless  # Operating in headless mode
+
 browser = Firefox(options=opts)
 
 browser.get('http://www.tsetmc.com/Loader.aspx?ParTree=15')
-
-
 search_form = browser.find_element_by_id('search_form_input')
-
 search_form.send_keys('نماد')
 search_form.submit
